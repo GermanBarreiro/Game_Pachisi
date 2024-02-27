7231a0da2eb615dbfe180ddcbe92b649344e0c84
grm4n-21 <germanbarreiro2121@gmail.com>
2024-02-27 09:30:30 -0500

    window_game

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print ("hola caras de penes ")
+ # importar las miles 
+import sys,pygame
+  # inicializar el game 
+pygame.init()
+
+#la ventana en que vamos a jugar  
+size =800,600 
+screen =pygame.display.set_mode(size)
+pygame.display.set_caption("Parchi")
+
+#ciclo infinito para  que solo se cierre si se preciona el cerrar
+run=True
+while run:
+    for event in pygame.event.get():
+        if event.type==pygame.QUIT:run=False
+
+pygame.quit       
