3dec5adf7ab2aa3cd332e86781f569d5201bd1f8
grm4n-21 <germanbarreiro2121@gmail.com>
2024-03-01 17:39:17 -0500

    fondo_listo

--- main.py
+++ main.py
@@ -1,17 +1,19 @@
-  # importar las miles 
+# importar las miles 
 import pygame
-  # inicializar el game 
+# inicializar el game 
 pygame.init()
-
+x=0
 #la ventana en que vamos a jugar  
 size =800,600 
-screen =pygame.display.set_mode(size)
-pygame.display.set_caption("Parchi")
+Pantalla=pygame.display.set_mode(size)
+wallpaper=pygame.image.load("data/fondo_2.png").convert()
+Pantalla.blit(wallpaper, (x,0))
 
-#ciclo infinito para  que solo se cierre si se preciona el cerrar
 run=True
 while run:
     for event in pygame.event.get():
         if event.type==pygame.QUIT:run=False
+    Pantalla.blit(wallpaper, (x,0))
+    pygame.display.flip()    
 
-pygame.quit       
+pygame.quit
