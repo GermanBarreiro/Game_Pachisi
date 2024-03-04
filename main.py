1c1d7e584dcdbf80a226f425d0190477b76be761
grm4n-21 <germanbarreiro2121@gmail.com>
2024-03-04 09:39:10 -0500

    wallpaper_mov

--- main.py
+++ main.py
@@ -3,17 +3,27 @@ import pygame
 # inicializar el game 
 pygame.init()
 x=0
+FPS=60
+Reloj=pygame.time.Clock()
 #la ventana en que vamos a jugar  
-size =800,600 
-Pantalla=pygame.display.set_mode(size)
+W,H =800,600 
+Pantalla=pygame.display.set_mode((W,H))
 wallpaper=pygame.image.load("data/fondo_2.png").convert()
+x=0
 Pantalla.blit(wallpaper, (x,0))
 
 run=True
 while run:
     for event in pygame.event.get():
         if event.type==pygame.QUIT:run=False
-    Pantalla.blit(wallpaper, (x,0))
-    pygame.display.flip()    
+    x_aux= x % Pantalla.get_rect().width   
+    Pantalla.blit(wallpaper, (x_aux-Pantalla.get_rect().width,0))
+    if x_aux< W :
+        Pantalla.blit(wallpaper,(x_aux,0))
+
+
+    x-=1    
+    pygame.display.flip()
+    Reloj.tick(FPS)    
 
 pygame.quit
