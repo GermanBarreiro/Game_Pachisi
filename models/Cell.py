c9c9e619d59ef8baf183e5bec2dda9bfac269713
grm4n-21 <germanbarreiro2121@gmail.com>
2024-03-05 08:47:41 -0500

    modulos_sin errores

--- models/Cell.py
+++ models/Cell.py
@@ -1,7 +1,7 @@
 class Cell:
     def __init__(self, indice, color, inicio, fin, proteccion):
-            self.indice = indice #numeros
-            self.color = color #string
-            self.inicio = inicio #buleano
-            self.fin = fin #buleano
-            self.proteccion = proteccion #buleano
+        self.indice = indice #numeros
+        self.color = color #string
+        self.inicio = inicio #buleano
+        self.fin = fin #buleano
+        self.proteccion = proteccion #buleano
